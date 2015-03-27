ff65bbca614875b51df97640046b5d82f248402e
Pablo Lluch <pablo.lluch@gmail.com>
2015-03-27 16:35:47 -0300

    Trim routes, add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,68 +1,16 @@
 Rails.application.routes.draw do
-  resources :report_fields
-  resources :report_field_types
-  resources :contacts
-  resources :venues
-  resources :report_states
-  resources :report_types
-  resources :pictures
-  resources :actions
-  resources :action_types
-  resources :reports
-  devise_for :users
-  resources :organizations
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  use_doorkeeper
+  resources :report_fields, only: [ :show, :index ]
+  resources :report_field_types, only: [ :show, :index ]
+  resources :contacts, only: [ :show, :index ]
+  resources :venues, only: [ :show, :index ]
+  resources :report_states, only: [ :show, :index ]
+  resources :report_types, only: [ :show, :index ]
+  resources :pictures, only: [ :show, :index, :create ]
+  resources :actions, only: [ :show, :index ]
+  resources :action_types, only: [ :show, :index ]
+  resources :reports, only: [ :show, :index, :create, :update ]
+  devise_for :users, skip: [:registrations, :confirmations], skip_helpers: true
+  resources :organizations, only: [ :show, :index ]
 end
