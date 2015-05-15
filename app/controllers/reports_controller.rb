87de9b4fbde1cae1ab3d95031f1361f4e579c5d3
Pablo Lluch <pablo.lluch@gmail.com>
2015-05-15 13:11:00 -0300

    Report update

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -25,6 +25,15 @@ class ReportsController < ApplicationController
     end
   end
 
+  def update
+    @report = Report.find(params[:id])
+    if @report.update_attributes(update_params)
+      render json: @report, status: :ok
+    else
+      render json: @report, status: :unprocessable_entity
+    end
+  end
+
   private
   def create_params
     params.require(:report).permit(:workspace_id, :venue_id, :title, 
@@ -33,5 +42,9 @@ class ReportsController < ApplicationController
       :region, :commune, :reference, :comment,
       pictures_attributes: [ :url, :comment ],
       report_fields_attributes: [ :report_field_type_id, :value ])
+  end
+
+  def update_params
+    params.require(:report).permit(:assigned_user_id, :comment, :report_state_id)
   end
 end
