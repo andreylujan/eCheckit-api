3b6cb2ee9b97af5a1a9b20bd9189d3ef4a396a9e
Pablo Lluch <pablo.lluch@gmail.com>
2015-05-19 22:35:23 -0300

    User name

--- app/serializers/report_action_serializer.rb
+++ app/serializers/report_action_serializer.rb
@@ -14,10 +14,14 @@
 
 class ReportActionSerializer < ActiveModel::Serializer
   attributes :id, :report_action_type_id, :user_id, :report_id, :created_at, :updated_at,
-  :data, :report_action_type_name, :report_state_id
+  :data, :report_action_type_name, :report_state_id, :user_name
 
   def report_action_type_name
   	object.report_action_type.name
+  end
+
+  def user_name
+  	object.user.name
   end
 
 end
