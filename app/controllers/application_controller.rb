423efec6239b719ffc9921d20e436b24cae1703b
Pablo Lluch <pablo.lluch@gmail.com>
2015-05-28 16:46:06 -0300

    Allow authorization header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
       headers['Access-Control-Allow-Origin'] = '*'
     end
     headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'X-AUTH-TOKEN, X-API-VERSION, X-Requested-With, Content-Type, Accept, Origin, X-Titanium-Id, charset'
+    headers['Access-Control-Allow-Headers'] = 'X-AUTH-TOKEN, X-API-VERSION, X-Requested-With, Content-Type, Accept, Origin, X-Titanium-Id, charset, Authorization'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
   end
