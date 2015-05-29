65bbc5f30c500a826b9220b38d957f71b914b825
Pablo Lluch <pablo.lluch@gmail.com>
2015-05-29 13:39:00 -0300

    Add picture to user json

--- app/serializers/workspace_user_serializer.rb
+++ app/serializers/workspace_user_serializer.rb
@@ -1,13 +1,7 @@
 
 
 class WorkspaceUserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :name
-
-  def accepted
-  end
-
-  def pending
-  end
-
+  attributes :id, :email, :first_name, :last_name, :name,
+    :picture
 
 end
