60c023293a9e99be887c1b9c26a91fccac6a193a
Pablo Lluch <pablo.lluch@gmail.com>
2015-07-02 13:20:03 -0300

    Channel create and index

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -3,7 +3,17 @@ class ChannelsController < ApplicationController
 	before_action :doorkeeper_authorize!
 
 	def create
-		
+
+		if params[:subchannels_attributes].present?
+			params[:channel][:subchannels_attributes] = params[:subchannels_attributes]
+		end
+		@channel = Channel.new(channel_attributes)
+		@channel.workspace = Workspace.find(params[:workspace_id])
+		if @channel.save
+			render json: @channel, status: :created
+		else
+			render json: @channel, status: :unprocessable_entity
+		end
 	end
 
 	def index
@@ -19,7 +29,7 @@ class ChannelsController < ApplicationController
 
 	def channel_attributes
 		params.require(:channel).permit(:name,
-			channels_attributes: [ :name, :_destroy, :id ])
+			subchannels_attributes: [ :name, :_destroy, :id ])
 	end
 
 end
