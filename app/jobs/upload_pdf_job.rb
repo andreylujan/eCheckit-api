888667c0b5e51f9fe9ff64e6d181d09653fb2514
Alvaro <alvaro.mc2@gmail.com>
2015-10-20 16:05:54 -0300

    test pdf report

--- app/jobs/upload_pdf_job.rb
+++ app/jobs/upload_pdf_job.rb
@@ -8,8 +8,9 @@ class UploadPdfJob < ActiveJob::Base
 		report = Report.find(report_id)
 		ac = ActionController::Base.new()
 		# html = ac.render_to_string('templates/report.html.erb', 
-		html = ac.render_to_string('templates/report2.html.erb', 
-				locals: { :@report => report })
+		html = ac.render_to_string('templates/report2.html.erb',
+			locals: { :@report => report },
+			orientation: 'Landscape')
 		pdf = WickedPdf.new.pdf_from_string(html)
 		url = Amazon.upload_pdf(pdf)
 		if not url.nil?
