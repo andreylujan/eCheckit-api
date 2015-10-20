59c35209d5d48451126a04de3167b0a913cc6394
Alvaro <alvaro.mc2@gmail.com>
2015-10-20 16:40:52 -0300

    pdf report

--- app/jobs/upload_pdf_job.rb
+++ app/jobs/upload_pdf_job.rb
@@ -8,13 +8,10 @@ class UploadPdfJob < ActiveJob::Base
 		report = Report.find(report_id)
 		ac = ActionController::Base.new()
 		# html = ac.render_to_string('templates/report.html.erb', 
-		# html = ac.render_to_string('templates/report2.html.erb',
-		# 	locals: { :@report => report },
-		# 	orientation: 'Landscape',)
-		# pdf = WickedPdf.new.pdf_from_string(html)
-
-		pdf = render_to_string pdf: "reporte", template: "templates/report2.html.erb", encoding: "UTF-8", locals: { :@report => report }, orientation: 'Landscape'
-
+		html = ac.render_to_string('templates/report2.html.erb',
+			locals: { :@report => report }),
+			orientation: 'Landscape'
+		pdf = WickedPdf.new.pdf_from_string(html)
 		url = Amazon.upload_pdf(pdf)
 		if not url.nil?
 			report.update_attribute :pdf, url
