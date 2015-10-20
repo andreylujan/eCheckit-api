d7f5bb604d97ed5c4729db7194ee419a34e094bf
Alvaro <alvaro.mc2@gmail.com>
2015-10-20 13:28:47 -0300

    test pdf report

--- app/jobs/upload_pdf_job.rb
+++ app/jobs/upload_pdf_job.rb
@@ -1,19 +1,19 @@
 class UploadPdfJob < ActiveJob::Base
-  queue_as :default
+	queue_as :default
 
-  require 'amazon'
+	require 'amazon'
 
-  def perform(report_id)
-    
-    report = Report.find(report_id)
-    ac = ActionController::Base.new()
-    # html = ac.render_to_string('templates/report.html.erb', 
-    html = ac.render_to_string('templates/pdfReport.html.erb', 
-        locals: { :@report => report })
-    pdf = WickedPdf.new.pdf_from_string(html)
-    url = Amazon.upload_pdf(pdf)
-    if not url.nil?
-      report.update_attribute :pdf, url
-    end
-  end
+	def perform(report_id)
+		
+		report = Report.find(report_id)
+		ac = ActionController::Base.new()
+		# html = ac.render_to_string('templates/report.html.erb', 
+		html = ac.render_to_string('templates/report2.html.erb', 
+				locals: { :@report => report })
+		pdf = WickedPdf.new.pdf_from_string(html)
+		url = Amazon.upload_pdf(pdf)
+		if not url.nil?
+			report.update_attribute :pdf, url
+		end
+	end
 end
