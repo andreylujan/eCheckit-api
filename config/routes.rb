b302614318e8b44be234ac034118ff4ee4eee898
Nicolás Cantó D <nicolascanto1@gmail.com>
2015-11-04 16:34:10 -0300

    update2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :products
   resources :clients
+  resources :works
   apipie
   match '/*path', to: 'application#cors_preflight_check', via: :options
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104160707) do
+ActiveRecord::Schema.define(version: 20151104162521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -371,6 +371,11 @@ ActiveRecord::Schema.define(version: 20151104160707) do
 
   create_table "widgets", force: :cascade do |t|
     t.text     "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "works", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
