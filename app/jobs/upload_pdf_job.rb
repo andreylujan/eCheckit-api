191558b96779b0525ad8d94b9a70b6551a5426c3
Alvaro <alvaro.mc2@gmail.com>
2015-11-05 19:09:26 -0300

    dom report

--- app/jobs/upload_pdf_job.rb
+++ app/jobs/upload_pdf_job.rb
@@ -9,6 +9,7 @@ class UploadPdfJob < ActiveJob::Base
 		ac = ActionController::Base.new()
 		# html = ac.render_to_string('templates/report.html.erb', 
 		html = ac.render_to_string('templates/report2.html.erb',
+		# html = ac.render_to_string('templates/' + report.workspace_id.to_s +'/report2.html.erb',
 			locals: { :@report => report })
 		pdf = WickedPdf.new.pdf_from_string(html, zoom: 0.8)
 		url = Amazon.upload_pdf(pdf)
