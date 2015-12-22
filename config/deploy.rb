56474f6ef1a8982197db4b480a1bc25f377ea5d6
Pablo Lluch <pablo.lluch@gmail.com>
2015-12-22 11:39:46 -0300

    Fix rbenv config

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,7 @@ set :keep_releases, 5
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.4'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} /home/ubuntu/.rbenv/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 set :rbenv_custom_path, '/home/ubuntu/.rbenv/bin/rbenv'
 
