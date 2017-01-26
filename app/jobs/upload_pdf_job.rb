4c5df92f446cc5b74a8de7793b1c6f92e23d53ab
Pablo Lluch <pablo.lluch@gmail.com>
2017-01-25 21:49:17 -0300

    E-mail fix

--- app/jobs/upload_pdf_job.rb
+++ app/jobs/upload_pdf_job.rb
@@ -21,10 +21,10 @@ class UploadPdfJob < ActiveJob::Base
         report_action.send_create_email
       end
       if report.contact_email.present? and report.contact_email.include? '@' # and report.client_name == "Ewin"
-        UserMailer.delay(queue: "dom_email").report_email(report_id, report.contact_email)
+        UserMailer.delay(queue: "dom_email").report_email(report_id, I18n.transliterate(report.contact_email))
       end
       creator_email = report.assigned_user.present? ? report.assigned_user.email : report.creator.email
-      UserMailer.delay(queue: "dom_email").report_email(report_id, creator_email)
+      UserMailer.delay(queue: "dom_email").report_email(report_id, I18n.transliterate(creator_email))
       UserMailer.delay(queue: "dom_email").report_email(report_id, "informes@dom.cl")
     end
   end
