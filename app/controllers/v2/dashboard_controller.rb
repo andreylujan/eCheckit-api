4baf0b1af9716f7a74b8b5a4bfbcde808f6597e4
Alejandro Rodriguez <alejandrorodriguezpena@gmail.com>
2017-11-21 16:32:16 -0300

    dashboard date

--- app/controllers/v2/dashboard_controller.rb
+++ app/controllers/v2/dashboard_controller.rb
@@ -52,7 +52,7 @@ class V2::DashboardController < ApplicationController
     end
 
     current_month_user_reports = filtered_reports.where("reports.created_at >= ? AND reports.created_at < ?",
-                                                        DateTime.now.beginning_of_month, DateTime.now.end_of_month)
+                                                        DateTime.now.beginning_of_month, DateTime.now.end_of_month + 1)
     .where.not(assigned_user_id: nil)
 
     current_month_reports_by_user = current_month_user_reports.group_by(&:assigned_user).map do |info|
