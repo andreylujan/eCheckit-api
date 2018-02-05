64f8524cffb7775728a1d4d9471d8f0dd31703ab
Santiago Morales <smorales@bildchile.com>
2018-02-05 16:57:55 -0300

    test fix column in csv file

--- app/controllers/v1/reports_controller.rb
+++ app/controllers/v1/reports_controller.rb
@@ -98,8 +98,8 @@ class V1::ReportsController < ApplicationController
     workspace_id = params.require(:workspace_id)
     workspace = Workspace.find(workspace_id)
     @reports = workspace.reports
-    Rails.logger.info "Format : #{respond_to}"
     respond_to do |format|
+      Rails.logger.info "Format : #{format}"
       format.json do
         reports_json = []
         @reports.each do |r|
